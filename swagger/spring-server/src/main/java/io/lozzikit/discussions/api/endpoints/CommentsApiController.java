85001ecbb66718921abe41d4bfb7b88c856c23fd
Nicolas Rod <ncls.rod@gmail.com>
2017-11-17 11:19:25 +0100

    Added signature to CommentsApiController from CommentsApi interface

--- swagger/spring-server/src/main/java/io/lozzikit/discussions/api/endpoints/CommentsApiController.java
+++ swagger/spring-server/src/main/java/io/lozzikit/discussions/api/endpoints/CommentsApiController.java
@@ -5,12 +5,16 @@ import io.lozzikit.discussions.api.model.CommentRequest;
 import io.lozzikit.discussions.api.model.CommentResponse;
 import io.lozzikit.discussions.entities.CommentEntity;
 import io.lozzikit.discussions.repositories.CommentRepository;
+import io.swagger.annotations.ApiParam;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,7 +28,7 @@ public class CommentsApiController implements CommentsApi {
     CommentRepository commentRepository;
 
     @Override
-    public ResponseEntity<List<CommentResponse>> commentsGet(Long articleID) {
+    public ResponseEntity<List<CommentResponse>> commentsGet( @NotNull @ApiParam(value = "The articleID the user want to list comment from", required = true) @RequestParam(value = "articleID", required = true) Long articleID) {
         List<CommentEntity> commentsEntities = commentRepository.findByArticleID((long) articleID);
 
         List<CommentResponse> responses = commentsEntities.stream()
@@ -44,7 +48,7 @@ public class CommentsApiController implements CommentsApi {
     }
 
     @Override
-    public ResponseEntity<Void> commentsPost(CommentRequest comment) {
+    public ResponseEntity<Void> commentsPost(@ApiParam(value = "The comment the user want to post" ,required=true ) @RequestBody CommentRequest comment){
         CommentEntity entity = toCommentEntity(comment);
         commentRepository.save(entity);
 
@@ -80,52 +84,4 @@ public class CommentsApiController implements CommentsApi {
 
         return response;
     }
-
-    /*
-    public ResponseEntity<Object> createFruit(@ApiParam(value = "", required = true) @Valid @RequestBody Fruit fruit) {
-        CommentEntity newFruitEntity = toFruitEntity(fruit);
-        commentRepository.save(newFruitEntity);
-        Long id = newFruitEntity.getId();
-
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentRequest().path("/{id}")
-                .buildAndExpand(newFruitEntity.getId()).toUri();
-
-        return ResponseEntity.created(location).build();
-    }
-
-
-    public ResponseEntity<List<Fruit>> getFruits() {
-        List<Fruit> fruits = new ArrayList<>();
-        for (CommentEntity fruitEntity : commentRepository.findAll()) {
-            fruits.add(toFruit(fruitEntity));
-        }
-
-        Fruit staticFruit = new Fruit();
-        staticFruit.setColour("red");
-        staticFruit.setKind("banana");
-        staticFruit.setSize("medium");
-        List<Fruit> fruits = new ArrayList<>();
-        fruits.add(staticFruit);
-
-        return ResponseEntity.ok(fruits);
-    }
-
-
-    private CommentEntity toFruitEntity(Fruit fruit) {
-        CommentEntity entity = new CommentEntity();
-        entity.setColour(fruit.getColour());
-        entity.setKind(fruit.getKind());
-        entity.setSize(fruit.getSize());
-        return entity;
-    }
-
-    private Fruit toFruit(CommentEntity entity) {
-        Fruit fruit = new Fruit();
-        fruit.setColour(entity.getColour());
-        fruit.setKind(entity.getKind());
-        fruit.setSize(entity.getSize());
-        return fruit;
-    }
-    */
 }
