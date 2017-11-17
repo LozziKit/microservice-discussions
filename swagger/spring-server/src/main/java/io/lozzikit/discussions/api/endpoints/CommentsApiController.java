64cc903b8cdc4cb96b21b61c1bb8328dcf19c0d8
Basile portable <basile.chatillon@heig-vd.ch>
2017-11-17 14:52:38 +0100

    Refactor du code pour déléguer les taches de l'API à un service.

--- swagger/spring-server/src/main/java/io/lozzikit/discussions/api/endpoints/CommentsApiController.java
+++ swagger/spring-server/src/main/java/io/lozzikit/discussions/api/endpoints/CommentsApiController.java
@@ -3,21 +3,20 @@ package io.lozzikit.discussions.api.endpoints;
 import io.lozzikit.discussions.api.CommentsApi;
 import io.lozzikit.discussions.api.model.CommentRequest;
 import io.lozzikit.discussions.api.model.CommentResponse;
+import io.lozzikit.discussions.api.services.CommentService;
 import io.lozzikit.discussions.entities.CommentEntity;
-import io.lozzikit.discussions.repositories.CommentRepository;
 import io.swagger.annotations.ApiParam;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.constraints.NotNull;
-import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2017-07-26T19:36:34.802Z")
 
@@ -25,65 +24,47 @@ import java.util.stream.Collectors;
 public class CommentsApiController implements CommentsApi {
 
     @Autowired
-    CommentRepository commentRepository;
+    CommentService commentService;
 
     @Override
-    public ResponseEntity<List<CommentResponse>> commentsGet( @NotNull @ApiParam(value = "The articleID the user want to list comment from", required = true) @RequestParam(value = "articleID", required = true) Long articleID) {
-        List<CommentEntity> commentsEntities = commentRepository.findByArticleID((long) articleID);
-
-        List<CommentResponse> responses = commentsEntities.stream()
-                .map(s -> toCommentResponse(s)).collect(Collectors.toList());
+    public ResponseEntity<List<CommentResponse>> commentsGet(@NotNull @ApiParam(value = "The articleID the user want to list comment from", required = true) @RequestParam(value = "articleID", required = true) Long articleID) {
+        List<CommentResponse> responses = commentService.getCommentFromArticleID(articleID);
 
         return ResponseEntity.ok(responses);
     }
 
     @Override
-    public ResponseEntity<Void> commentsIdDelete(Long id) {
-        return null;
+    public ResponseEntity<Void> commentsIdDelete(@ApiParam(value = "ID of the comment we want to delete", required = true) @PathVariable("id") Long id) {
+        if(!commentService.commentExist(id)){
+            return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+        }
+
+        commentService.deleteComment(id);
+
+        return new ResponseEntity<Void>(HttpStatus.OK);
     }
 
     @Override
-    public ResponseEntity<CommentResponse> commentsIdPut(Long id) {
-        return null;
+    public ResponseEntity<CommentResponse> commentsIdPut(@ApiParam(value = "ID of the comment we want to edit.", required = true) @PathVariable("id") Long id) {
+        if(!commentService.commentExist(id)){
+            return new ResponseEntity<CommentResponse>(HttpStatus.NOT_FOUND);
+        }
+        if(!commentService.commentExist(id)){
+            return new ResponseEntity<CommentResponse>(HttpStatus.NOT_FOUND);
+        }
+
+
+        return new ResponseEntity<CommentResponse>(HttpStatus.OK);
     }
 
     @Override
-    public ResponseEntity<Void> commentsPost(@ApiParam(value = "The comment the user want to post" ,required=true ) @RequestBody CommentRequest comment){
-        if(comment.getMessage() == null){
+    public ResponseEntity<Void> commentsPost(@ApiParam(value = "The comment the user want to post", required = true) @RequestBody CommentRequest comment) {
+        if (!commentService.containsMessage(comment)) {
             return new ResponseEntity<Void>(HttpStatus.UNPROCESSABLE_ENTITY);
         }
-        CommentEntity entity = toCommentEntity(comment);
-        commentRepository.save(entity);
+
+        commentService.addNewComments(comment);
 
         return new ResponseEntity<Void>(HttpStatus.CREATED);
-    }
-
-    private CommentEntity toCommentEntity(CommentRequest comment) {
-        CommentEntity entity = new CommentEntity();
-        entity.setArticleID(comment.getArticleID());
-        entity.setAuthor(comment.getAuthor());
-        entity.setAuthorID(comment.getAuthorID());
-        entity.setMessage(comment.getMessage());
-
-        if (comment.getParentID() != null)
-            entity.setParent(commentRepository.findOne(comment.getParentID()));
-
-        return entity;
-    }
-
-    public CommentResponse toCommentResponse(CommentEntity comment) {
-        CommentResponse response = new CommentResponse();
-
-        response.setArticleID(comment.getArticleID());
-        response.setAuthor(comment.getAuthor());
-        response.setAuthorID(comment.getAuthorID());
-        response.setDate(new DateTime(comment.getDate()));
-        response.setId(comment.getId());
-        response.setMessage(comment.getMessage());
-
-        if (comment.getParent() != null)
-            response.setParentID(comment.getParent().getId());
-
-        return response;
     }
 }
---
+++ swagger/spring-server/src/main/java/io/lozzikit/discussions/api/services/CommentService.java
@@ -0,0 +1,115 @@
+package io.lozzikit.discussions.api.services;
+
+import io.lozzikit.discussions.api.model.CommentRequest;
+import io.lozzikit.discussions.api.model.CommentResponse;
+import io.lozzikit.discussions.entities.CommentEntity;
+import io.lozzikit.discussions.repositories.CommentRepository;
+import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@SuppressWarnings("Duplicates")
+@Component
+public class CommentService {
+    @Autowired
+    CommentRepository commentRepository;
+
+    public List<CommentResponse> getCommentFromArticleID(long articleID) {
+        List<CommentEntity> commentsEntities = commentRepository.findByArticleID((long) articleID);
+
+        return toCommentResponse(commentsEntities);
+    }
+
+    public boolean containsMessage(CommentRequest commentRequest) {
+        String message = commentRequest.getMessage();
+        return message != null && !message.isEmpty();
+    }
+
+    public void addNewComments(CommentRequest commentRequest) {
+        CommentEntity entity = toCommentEntity(commentRequest);
+        commentRepository.save(entity);
+    }
+
+    public boolean commentExist(long commentID){
+        return commentRepository.exists(commentID);
+    }
+
+    public void deleteComment(long commentID){
+        CommentEntity commentEntity = commentRepository.findOne(commentID);
+        if(commentEntity.isLeaf()){
+            commentRepository.delete(commentEntity);
+        }
+        else{
+            commentEntity.setMessage("");
+            commentRepository.save(commentEntity);
+        }
+    }
+
+    private CommentEntity toCommentEntity(CommentRequest comment) {
+        CommentEntity entity = new CommentEntity();
+        entity.setArticleID(comment.getArticleID());
+        entity.setAuthor(comment.getAuthor());
+        entity.setAuthorID(comment.getAuthorID());
+        entity.setMessage(comment.getMessage());
+
+        if (comment.getParentID() != null)
+            entity.setParent(commentRepository.findOne(comment.getParentID()));
+
+        return entity;
+    }
+
+    private List<CommentEntity> toCommentEntity(List<CommentRequest> comments) {
+        return comments.stream()
+                .map(s -> toCommentEntity(s))
+                .collect(Collectors.toList());
+    }
+
+    public CommentResponse toCommentResponse(CommentEntity comment) {
+        CommentResponse response = new CommentResponse();
+
+        response.setArticleID(comment.getArticleID());
+        response.setAuthor(comment.getAuthor());
+        response.setAuthorID(comment.getAuthorID());
+        response.setDate(new DateTime(comment.getDate()));
+        response.setId(comment.getId());
+        response.setMessage(comment.getMessage());
+
+        if (comment.getParent() != null)
+            response.setParentID(comment.getParent().getId());
+
+        return response;
+    }
+
+    private List<CommentResponse> toCommentResponse(List<CommentEntity> comments) {
+        return comments.stream()
+                .map(s -> toCommentResponse(s))
+                .collect(Collectors.toList());
+    }
+
+    private CommentRequest CommentResponseToRequest(CommentResponse response) {
+        CommentRequest commentRequest = new CommentRequest();
+        return commentRequest;
+    }
+
+    private CommentResponse CommentRequestToResponse(CommentRequest request) {
+        CommentResponse commentRequest = new CommentResponse();
+        return commentRequest;
+    }
+
+    private List<CommentRequest> ListCommentResponseToRequest(List<CommentResponse> listResponse) {
+        return listResponse.stream()
+                .map(s -> CommentResponseToRequest(s))
+                .collect(Collectors.toList());
+    }
+
+    private List<CommentResponse> ListCommentRequestToResponse(List<CommentRequest> listRequest) {
+        return listRequest.stream()
+                .map(s -> CommentRequestToResponse(s))
+                .collect(Collectors.toList());
+    }
+
+
+}
