9777600d3bb077e12ae908fd48d72c2d9a33c94d
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2018-01-19 16:37:12 +0100

    Adding JWTUtils

---
+++ swagger/spring-server/src/main/java/io/lozzikit/discussions/util/JWTUtils.java
@@ -0,0 +1,22 @@
+package io.lozzikit.discussions.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+/**
+ * Simplified from https://www.toptal.com/java/rest-security-with-jwt-spring-security-and-java
+ */
+public class JWTUtils {
+
+    /**
+     * Parse a token and get the associated userId
+     */
+    public long getUserId(String token) {
+        // We are not checking signature for now
+        Claims claims = Jwts.parser().parseClaimsJws(token).getBody();
+
+
+        return claims.get("userid", Long.class);
+    }//*/
+
+}
